eb7f4566e5f3719e30292e84ad372d044ff6911a
Lucas Fidelis <lucasm241301@gmail.com>
2026-07-25 09:31:11 -0300

    test: configura collectCoverageFrom e coverageThreshold para o jest

--- jest.config.js
+++ jest.config.js
@@ -12,4 +12,43 @@ export default {
   moduleNameMapper: {
     '^(\\.{1,2}/.*)\\.js$': '$1',
   },
+  collectCoverageFrom: ['./src/**/*'],
+  coveragePathIgnorePatterns: [
+    './src/generated',
+    './src/tests',
+    './src/types',
+    './src/schemas/error.schema.ts',
+    './src/schemas/health.schema.ts',
+    './src/plugins/swagger.ts',
+    './src/index.ts',
+    './src/lib/prisma.ts',
+    './src/utils/swagger.tags.ts',
+    './src/routes',
+  ],
+  coverageThreshold: {
+    global: {
+      statements: 65,
+      branches: 55,
+      functions: 60,
+      lines: 65,
+    },
+    './src/services/**/*.ts': {
+      statements: 90,
+      branches: 85,
+      functions: 90,
+      lines: 90,
+    },
+    './src/utils/security/**/*.ts': {
+      statements: 90,
+      branches: 85,
+      functions: 90,
+      lines: 90,
+    },
+    './src/utils/errors/**/*.ts': {
+      statements: 90,
+      branches: 85,
+      functions: 90,
+      lines: 90,
+    },
+  },
 }
